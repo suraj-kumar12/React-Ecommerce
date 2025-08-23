39c2840a55a4f2f1e5440dfdf2e3af0ca6318e3e
surajkumar <surajroman2588@gmail.com>
2025-08-23 15:32:11 +0530

    third commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  // base: "/React-Ecommerce/",
+  base: "/",
   plugins: [react(), tailwindcss()],
 });
